3ddde474a093878940a1e11f78f9e4dc7b9c4912
zdub0is <az.duboiz@gmail.com>
2023-03-24 14:34:18 -0700

    Starting container writing

--- src/tabs/Home.js
+++ src/tabs/Home.js
@@ -1,8 +1,22 @@
+import { Container, Button, Row } from 'react-bootstrap';
+
 function Home() {
     return (
-        <div>
+        <Container className="Home d-flex bg-dark text-light bg">
+        <Row>
+            <Container className='text-center myCard mx-3 p-2'>
+                <h1>Comparison of Multiple AIs</h1>
+                <p>Lorem ipsum</p>
+                <Row>
+                <Button>Portfolio</Button>
+                <Button>Github</Button>
+                </Row>
+            </Container>
+        </Row>
+        <Row md={2}>
             
-        </div>
+        </Row>
+        </Container>
     )
 }
 
